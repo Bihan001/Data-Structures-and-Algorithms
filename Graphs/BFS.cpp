0c5cedafc9360934413557b23dba24c37f10fc53
Bihan Chakraborty <bhchakraborty001@gmail.com>
2020-11-20 23:24:16 +0530

    bfs modification

--- Graphs/BFS.cpp
+++ Graphs/BFS.cpp
@@ -19,12 +19,17 @@ class Graph {
     vector<Vertex> vertexList;
 
    public:
+    int getVertexListSize() {
+        return vertexList.size();
+    }
     void addVertex(string data) {
         vertexList.push_back(Vertex(data));
     }
     void set_edge(int u, int v) {
         vertexList[u].neighbours.push_back(v);
     }
+
+    // Normal BFS
     void bfs() {
         queue<int> q;
         vector<bool> visited(vertexList.size(), false);
@@ -46,6 +51,56 @@ class Graph {
             }
         }
     }
+    // Calculates distance from root node...assuming its undirected and tree like
+    vector<int> bfs_calc_distance() {
+        queue<int> q;
+        vector<bool> visited(vertexList.size(), false);
+        vector<int> distance(vertexList.size(), -1);
+        for (int i = 0; i < vertexList.size(); i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                q.push(i);
+                distance[i] = 0;
+                while (!q.empty()) {
+                    int p = q.front();
+                    q.pop();
+                    for (int x : vertexList[p].neighbours) {
+                        if (!visited[x]) {
+                            visited[x] = true;
+                            q.push(x);
+                            distance[x] = distance[p] + 1;
+                        }
+                    }
+                }
+            }
+        }
+        return distance;
+    }
+
+    // Calculates parent of each ndoe...assuming its undirected
+    vector<int> bfs_calc_parent() {
+        queue<int> q;
+        vector<bool> visited(vertexList.size(), false);
+        vector<int> parent(vertexList.size(), -1);
+        for (int i = 0; i < vertexList.size(); i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                q.push(i);
+                while (!q.empty()) {
+                    int p = q.front();
+                    q.pop();
+                    for (int x : vertexList[p].neighbours) {
+                        if (!visited[x]) {
+                            visited[x] = true;
+                            q.push(x);
+                            parent[x] = p;
+                        }
+                    }
+                }
+            }
+        }
+        return parent;
+    }
 };
 
 int main() {
@@ -66,5 +121,25 @@ int main() {
     g.set_edge(3, 4);
     g.set_edge(6, 7);
     g.bfs();
+    cout << endl;
+    vector<int> dist = g.bfs_calc_distance();
+    for (auto it : dist) cout << it << " ";
+    cout << endl;
+    vector<int> par = g.bfs_calc_parent();
+    for (auto it : par) cout << it << " ";
+    cout << endl;
+    for (int i = 0; i < g.getVertexListSize(); i++) {
+        cout << i << "\t";
+        int cur = par[i];
+        deque<int> path;
+        path.push_front(i);
+        while (cur != -1) {
+            path.push_front(cur);
+            cur = par[cur];
+        }
+        for (auto it : path) cout << it << " ";
+        path.clear();
+        cout << endl;
+    }
     return 0;
 }
